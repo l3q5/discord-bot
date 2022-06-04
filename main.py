b357cbdb55042ff3cbb60e91267a7184f97833b4
Caio Duarte <l3q5@proton.me>
2022-06-04 16:48:24 -0300

    on_message hook added.

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import discord
-import random
-
-TOKEN = "NDY3ODg4NDU4NTUyMDQ5Njg0.GsgWSP.Do7eH5ubKeVIZ9jHjeEFVnzx9etIfFJuVcdD18"
+from tolkien import TOKEN
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('logged in ')
+    print("logged in ")
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split("#")[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f"{username}: {user_message} ({channel})")
+
+    if message.author == client.user:
+        return
+
+    if message.channel.name == "bot":
+        if user_message.lower() == "h":
+            await message.channel.send(f"oieee {username}!")
+            return
+        elif user_message.lower() == "cu podre":
+            await message.channel.send(f"bluezaosso {username}!")
+            return
 
 client.run(TOKEN)
